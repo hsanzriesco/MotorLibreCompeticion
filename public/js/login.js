5e46828e180e424cf5818187ff2fffeb34b81d20
hsanzriesco <hsanzriesco@gmail.com>
2025-10-20 15:59:47 +0200

    modificacion login.js /api

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value;
 
   try {
-    const res = await fetch("/api/loginUser", {
+    const res = await fetch("/api/createUser", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
