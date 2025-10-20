8e7447f3759e9068fea52db7bee29a274f16509a
hsanzriesco <hsanzriesco@gmail.com>
2025-10-20 18:06:30 +0200

    a

--- public/pages/auth/register/register.js
+++ public/pages/auth/register/register.js
@@ -4,26 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
+    const name = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
-
-    if (!username || !email || !password) {
-      alert("⚠️ Todos los campos son obligatorios");
-      return;
-    }
 
     try {
       const res = await fetch("/api/createUser", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ name, email, password }),
       });
 
       const result = await res.json();
 
       if (result.success) {
-        alert("✅ Usuario registrado correctamente");
+        alert("✅ Usuario creado con éxito");
         window.location.href = "../login/login.html";
       } else {
         alert("⚠️ " + result.message);
