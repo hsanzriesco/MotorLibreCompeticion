c478577795a288497332d61b1ca3e5d061b549d1
hsanzriesco <hsanzriesco@gmail.com>
2025-10-20 16:49:40 +0200

    cambio de fetch en register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,30 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("registerForm");
+    const form = document.getElementById("registerForm");
 
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
+        const username = document.getElementById("username").value.trim();
+        const password = document.getElementById("password").value.trim();
 
-    try {
-      const res = await fetch("/api/createUser", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+        try {
+            const res = await fetch("https://motor-libre-competicion.vercel.app/api/createUser", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password }),
+            });
 
-      const result = await res.json();
 
-      if (result.success) {
-        alert("✅ Usuario creado con éxito");
-        window.location.href = "../login/login.html";
-      } else {
-        alert("⚠️ " + result.message);
-      }
-    } catch (err) {
-      alert("❌ Error al conectar con el servidor");
-      console.error(err);
-    }
-  });
+            const result = await res.json();
+
+            if (result.success) {
+                alert("✅ Usuario creado con éxito");
+                window.location.href = "../login/login.html";
+            } else {
+                alert("⚠️ " + result.message);
+            }
+        } catch (err) {
+            alert("❌ Error al conectar con el servidor");
+            console.error(err);
+        }
+    });
 });
