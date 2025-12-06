b4cb234d72771410fa57d8727ffd7adc9ac88c50
hsanzriesco <hsanzriesco@gmail.com>
2025-12-06 17:58:16 +0100

    footer.js con imagen integrada

--- public/footer/footer.js
+++ public/footer/footer.js
@@ -7,19 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
     // Le asignamos el ID que usas en el CSS: #main-footer
     footerElement.id = 'main-footer';
 
-    // 2. Definir el contenido HTML sin la línea de derechos de autor
+    // 2. Definir el contenido HTML. Se reemplaza el texto de la primera columna por la imagen.
     footerElement.innerHTML = `
         <div class="footer-content container py-5">
             <div class="row">
                 
-                <div class="col-md-4 mb-3">
-                    <h5 class="text-danger fw-bold">Motor Libre Competición</h5>
+                <div class="col-md-4 mb-4 text-center">
+                    
+                    <a href="./index.html" class="d-inline-block">
+                        <img src="./img/imagen-tfg.png" alt="Motor Libre Competición Logo" height="80" class="img-fluid mb-2">
+                    </a>
+                    
+                    <h5 class="text-danger fw-bold mt-2">Motor Libre Competición</h5>
                     <p class="text-white-50">
                         La plataforma oficial para gestionar competiciones y eventos de motor en tu región.
                     </p>
                 </div>
                 
-                <div class="col-md-4 mb-3">
+                <div class="col-md-4 mb-4 text-center text-md-start">
                     <h5 class="text-danger fw-bold">Navegación</h5>
                     <ul class="list-unstyled">
                         <li><a href="./index.html" class="text-light text-decoration-none">Inicio</a></li>
@@ -29,11 +34,11 @@ document.addEventListener("DOMContentLoaded", () => {
                     </ul>
                 </div>
 
-                <div class="col-md-4 mb-3">
+                <div class="col-md-4 mb-4 text-center text-md-end">
                     <h5 class="text-danger fw-bold">Síguenos</h5>
                     <a href="#" class="text-light me-3"><i class="bi bi-facebook fs-4"></i></a>
                     <a href="#" class="text-light me-3"><i class="bi bi-twitter fs-4"></i></a>
-                    <a href="#" class="text-light me-3"><i class="bi bi-instagram fs-4"></i></a>
+                    <a href="#" class="text-light"><i class="bi bi-instagram fs-4"></i></a>
                 </div>
             </div>
             
